1cfd55182d4f8ed32466fa1f7c263606d3ee748e
Angel Urbano <angelurbano@macbookpro.home>
2026-09-22 14:45:35 +0200

    Add joke-of-the-day functionality

--- todo_list.py
+++ todo_list.py
@@ -1,5 +1,14 @@
 import streamlit as st
 from task import Task
+from jokes import generate_joke
+
+    
+api_key = st.secrets["jokes_api"]["api_key"]
+
+if "joke" not in st.session_state:
+    st.session_state.joke = generate_joke(api_key)
+    
+st.info(st.session_state.joke)
 
 if "task_list" not in st.session_state:
     st.session_state.task_list = []
